d658c364c1924d46c74da0200f4d1f7c95a81b14
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-05-05 23:20:36 +0300

    Update index.go

--- index.go
+++ index.go
@@ -3,12 +3,11 @@ package main
 import (
   "net/http"
   "./urls"
-  "./setup"
 )
 
 func main () {
 
   http.HandleFunc ("/", urls.Index)
-  http.ListenAndServe (setup.PORT, nil)
+  http.ListenAndServe (":8000", nil)
 
 }
